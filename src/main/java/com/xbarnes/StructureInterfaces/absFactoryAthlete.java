6ddbaabcd86ab47c0c922d32a7aa05734438a292
xbarn <xbarn@LAPTOP-R1RSHDCP.home>
2020-11-07 18:00:09 +0100

    Continue with project structure and add api conenction to get info from the server

---
+++ src/main/java/com/xbarnes/StructureInterfaces/absFactoryAthlete.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.xbarnes.StructureInterfaces;
+
+import com.xbarnes.BaseClasses.Athlete;
+
+/**
+ *
+ * @author xbarn
+ */
+public interface absFactoryAthlete {
+    
+    public Athlete createAthlete(int id, String compName, String lastName, boolean status, char gender, String countryOfOrigin, String countryOfOriginCode, int divisionId, int affiliateID, String affiliateName, int age, String height, String weight);
+    
+}
